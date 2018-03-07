57ab0ed67a2946bde8c95c02ce189553d822cf90
peachR <405891205@qq.com>
2018-03-07 14:44:54 +0800

    Listp接口

---
+++ src/main/java/com/mavenSSM/model/Listp.java
@@ -0,0 +1,11 @@
+package com.mavenSSM.model;
+
+public interface Listp {
+	 public String getEffectiveness() ;
+
+	 public void setEffectiveness(String Effectiveness);
+	    
+	 public String getStability() ;
+	    
+	 public void setStability(String Stability) ;
+}
